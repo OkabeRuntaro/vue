b478b4b95b03ae6c28b60f85c1ee7cbdad665a09
OkabeRuntaro <992536005@qq.com>
2024-10-24 22:57:35 +0800

    one

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- html -->
+     <div class="app">
+        <h1>你哈</h1>
+        <Person></Person>
+     </div>
+</template>
+
+<script lang="ts">
+//JS/TS
+    import Person from './components/Person.vue';
+    export default{
+    name:"App",  //组件名字
+    components:{Person}
+    }
+</script>
+
+<style>
+/*样式*/
+.app{
+     background-color: bisque;
+}
+</style>
+
---
+++ src/components/Person.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- html -->
+     <div class="person">
+         <h2>姓名：{{ person.name }}</h2>
+         <h2>年龄：{{ person.age }}</h2>
+         <button @click="chengeName">修改名字</button>
+         <button @click="chengeAge">修改年龄</button>
+     </div>
+</template>
+
+
+
+<script lang="ts" setup name = 'Person'>
+    import {reactive, ref} from 'vue';
+
+    let person = reactive({
+        name : '张三',
+        age :18
+    })
+
+    function chengeName(){
+        person.name += '~'
+    }
+    function chengeAge(){
+        person.age += 1
+    }
+
+</script>
+
+<style scoped>
+/*样式*/
+.person{
+     background-color: skyblue;
+}
+</style>
+
